b788a25dc7fce030f46578997e40745891241d36
VanderSVan <101255612+VanderSVan@users.noreply.github.com>
2022-03-23 13:28:13 +0300

    upgrade functional tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,12 @@
 import pytest
-from api_report import create_app
+from peewee import SqliteDatabase
+from api_report.app import create_app
+from api_report.db.models import Driver, Team, RaceInfo
+from api_report.db.insert_records import db_data_prepare
+
+MODELS = [Driver, Team, RaceInfo]
+test_db = SqliteDatabase(':memory:')
+data_list = db_data_prepare()
 
 
 @pytest.fixture
@@ -9,5 +16,22 @@ def app():
 
 @pytest.fixture
 def client(app):
+    app.config['DEBUG'] = False
     return app.test_client()
 
+
+@pytest.fixture(scope='class')
+def connect_test_db():
+    """Fixture to connect and disconnect database for each test class"""
+    test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
+    with test_db:
+        print('---open---')
+        # setup
+        test_db.drop_tables(MODELS)
+        test_db.create_tables(MODELS)
+        for table, data in zip(MODELS, data_list):
+            data_insert_query = table.insert_many(data)  # insert data to db
+            data_insert_query.execute()
+        yield
+        # teardown
+        print('---close---')
